a741b0d05eb0a71ca94219a52262ce5eea2029ab
Yadu-M <ymadhu@torontomu.ca>
2024-04-02 13:23:26 -0400

    added database (tindy-db) and added add user and auth methods

---
+++ utils.py
@@ -0,0 +1,36 @@
+from tinydb import TinyDB, Query
+
+DB_PATH = 'db.json'
+db = TinyDB(DB_PATH)
+
+def add_user(username, password):
+    try:
+        query = Query()
+        check = db.contains(query.username == username)
+        if check:
+            raise Exception("User already exists")
+        else:
+            db.insert({"username": username,
+                       "pass": password})
+            return "success"
+    except Exception as e:
+        return Exception(e)
+
+def auth(username, password):
+    try:
+        User = Query()
+        account = db.search(User.username == username)        
+        if (len(account) == 0):
+            raise Exception("No account found lil bro")
+        
+        print(account[0]['pass'])
+        if account[0]['pass'] == password:
+            return "success"
+        else:
+            raise Exception("Wrong password detected")
+        
+    except Exception as e:
+        return Exception(e)
+        
+        
+auth('Bob', 'asda')
